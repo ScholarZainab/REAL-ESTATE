62cafab3d705acd5ae7721ef6ebd31cd454a8315
Scholar Zainab <140726679+ScholarZainab@users.noreply.github.com>
2024-12-08 08:18:58 +0100

    Create settings.py

---
+++ src/components/settings.py
@@ -0,0 +1,7 @@
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Use your email provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
